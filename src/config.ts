01edc3ffa630aca0e9d3b2f4734475650e681ca0
wanghao22 <10504676+wanghao22@user.noreply.gitee.com>
2025-08-07 21:47:11 +0800

    updata notes

--- src/config.ts
+++ src/config.ts
@@ -59,7 +59,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/wang-hao-byte",
 		},
 	],
 };
